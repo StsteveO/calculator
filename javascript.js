13e8a0ca450de1a76a23d7b4a19db0ad5aae82a9
steve omari <steveomari62@gmail.com>
2022-12-06 07:21:49 -0500

    update equation screen& operation buttons

--- javascript.js
+++ javascript.js
@@ -50,12 +50,38 @@ let operate=(num1,operator,num2)=>{
 console.log(operate(75,'/',0));
 
 let equationScreen= document.querySelector('.equation-screen');
+// let equationArr= equationScreen.textContent=[];
+let equationArr=[];
+
 
 window.addEventListener('click', function(e){
-    let readOut=(e.path[0].localName); //ex. 'button'
-    if(readOut!=='button'){
+    let sectionClicked=(e.path[0].localName); //ex. 'button'
+    if(sectionClicked!=='button'){
         return
-    }else{
-        equationScreen.textContent=(e.target.outerText); //ex. '6'
+    }
+    let buttonClicked=e.target.outerText; //ex. '6'
+    // console.log(`${buttonClicked} is the burron clicked`);
+
+    equationArr.push(buttonClicked);
+    console.log(equationArr);//array of numbers 
+
+    let checkForSign=(button)=>{
+        return (button==='+'||button==='-'||button==='x'||button==='/');
     };
+
+    let joinedNumbers= equationArr.join('');//joins numbers to make string
+    equationScreen.textContent= joinedNumbers;
+
+    let indexOfSign= equationArr.findIndex(checkForSign);
+    if(indexOfSign<0){
+        return
+    };
+    console.log(`${indexOfSign} is the index of the first operator`);//returns index of first operator    
 });
+
+//when user clicks operator, seperate the array between number-operator-number.
+//find out which operator user clicked
+//put it in readout form
+//plug it into operation() function
+
+//operate(num1,(indexOfSign), num2)
